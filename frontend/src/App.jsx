0f2179d273b15919fb57b6fb52935150b2f89016
Alex <mohammad.mohammad@dci-student.org>
2024-07-22 09:10:03 +0200

    header1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,18 +5,19 @@ import Home from "./pages/Home/Home";
 import Kontakt from "./pages/Kontakt/Kontakt";
 import NotFound from "./pages/NotFound/NotFound";
 import "./css/App.css";
-import Card from "./components/Card/Card";
+import Header1 from "./components/Header1/Header1";
 function App() {
     return (
         <>
             <AppProvider>
                 <BrowserRouter>
                     <Routes>
+                        <Route path="header1" element={<Header1 />} />
                         <Route path="/" element={<SharedLayout />}>
                             <Route index element={<Home />} />
                             {/* Weitere Routen hier zwischen */}
                             <Route path="kontakt" element={<Kontakt />} />
-                            <Route path="card" element={<Card />} />
+                            
                             {/* Weitere Routen hier zwischen */}
                             <Route path="*" element={<NotFound />} />
                         </Route>
--- frontend/src/components/Card/Card.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Card() {
-  return (
-    <div>
-      <h1>Ali</h1>
-    </div>
-  )
-}
