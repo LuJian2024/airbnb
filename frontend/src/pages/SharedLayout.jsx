111cf84e5ff89ddd0ffd4ef572dd3a13c382b986
JianLu <jian.lu@dci-student.org>
2024-07-22 15:40:48 +0200

    refactor: Remove commented out code and update header component in SharedLayout.jsx

--- frontend/src/pages/SharedLayout.jsx
+++ frontend/src/pages/SharedLayout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
-import Header1 from "../components/Header1/Header1";
+// import Header1 from "../components/Header1/Header1";
+import Menu from "../components/Menu"
 
 const SharedLayout = () => {
     return (
         <>
             {/* nav/header */}
             {/* <h2>Ich bin die zukünftige Navbar</h2> */}
-            <Header1 />
+            <Menu />
             <main className="main-container">
                 <Outlet />
             </main>
