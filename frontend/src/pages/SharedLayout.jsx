22105a05ee4106d05fd1c83df952fcba89e16c85
Alex <mohammad.mohammad@dci-student.org>
2024-07-24 13:54:39 +0200

    font-error

--- frontend/src/pages/SharedLayout.jsx
+++ frontend/src/pages/SharedLayout.jsx
@@ -5,16 +5,20 @@ import Search from "../components/Search/Search"
 
 const SharedLayout = () => {
     return (
-        <>
+        <><div className="body-part">
             {/* nav/header */}
             {/* <h2>Ich bin die zukünftige Navbar</h2> */}
-            <Menu />
-            <Search />
-            <main className="main-container">
+           <div className="menu-part"> <Menu />
+            </div>
+            <div className="search-part"><Search /></div>
+            
+            <div className="main-part"><main className="main-container">
                 <Outlet />
             </main>
             <h2>Ich bin der zukünftige Footer</h2>
             {/* footer */}
+            </div>
+           </div>
         </>
     );
 };
